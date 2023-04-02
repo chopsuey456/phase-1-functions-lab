2b4713eca8993fea996c6799e0eff7fe3944b617
Sara Withrow <saralouise1107@gmail.com>
2023-04-02 16:09:05 -0400

    new

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(valueOne){
+   let startingPointInBlocks = Math.abs(valueOne - 42);
+   return startingPointInBlocks;
+};
+
+function distanceFromHqInFeet(valueOne){
+    let convertToFeet = distanceFromHqInBlocks(valueOne) * 264;
+    return convertToFeet;
+};
+
+function distanceTravelledInFeet(start, destination){
+    let convertToFeet = Math.abs((destination - start) * 264);
+    return convertToFeet;
+
+};
+
+function calculatesFarePrice(start, destination){
+    let farePriceDistance = Math.abs(distanceTravelledInFeet(start, destination))
+    
+
+    if(farePriceDistance > 2500){
+        return 'cannot travel that far'
+    } else if(farePriceDistance > 2000){
+        return 25
+    } else if(farePriceDistance > 400){
+        return (farePriceDistance - 400) * 0.02
+    } else if(farePriceDistance < 400){
+        return 0
+    }
+
+};
+
